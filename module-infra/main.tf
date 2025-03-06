f56edeab04d60ee1840b9cde1088158a69610ae3
santoshpawar02 <santosh.pawar02@gmail.com>
2025-03-06 15:50:42 +0530

    vault tools add

---
+++ module-infra/main.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "tool" {
+  name = "${var.name}-sg"
+  description = "${var.name} Security Group"
+
+  tags = {
+    name = "${var.name}-sg" 
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.tool.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.tool.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = var.port
+  ip_protocol = "tcp"
+  to_port = var.port
+}
+
+
+resource "aws_instance" "tool" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.tool.id]
+
+  tags = {
+    name = var.name  
+  }
+}
+
+
+resource "aws_route53_record" "private" {
+  zone_id = var.zone_id
+  name    = "${var.name}-private"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance.private_ip]
+}
+
+resource "aws_route53_record" "public" {
+  zone_id = var.zone_id
+  name    = var.name
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance.public_ip]
+}
---
+++ module-infra/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {}
+
+variable "zone_id" {}
+variable "name" {}
+variable "instance_type" {}
+variable "port" {}
+variable "zone_id" {}
