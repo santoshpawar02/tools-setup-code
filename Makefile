afd784702402c10ef53ae97797560c531aa82bae
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-03-07 20:48:21 +0530

    code check

--- Makefile
+++ Makefile
@@ -1,31 +1,12 @@
 infra:
 	git pull
-	terraform init 
-	terraform apply -auto-approve 
+	terraform init
+	terraform apply -auto-approve
+
 ansible:
 	git pull
 	ansible-playbook -i $(tool_name)-internal.santoshpawar.site, setup-tool.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e tool_name=$(tool_name)
 
-#dev-apply:
-# 	git pull
-# 	rm -f .terraform/terraform.tfstate
-# 	terraform init -backend-config=env-dev/state.tfvars
-# 	terraform apply -auto-approve -var-file=env-dev/main.tfvars -var vault_token=$(vault_token)
-
-# dev-destroy:
-# 	git pull
-# 	rm -f .terraform/terraform.tfstate
-# 	terraform init -backend-config=env-dev/state.tfvars
-# 	terraform destroy -auto-approve -var-file=env-dev/main.tfvars -var vault_token=$(vault_token)
-
-# prod-apply:
-# 	git pull
-# 	rm -f .terraform/terraform.tfstate
-# 	terraform init -backend-config=env-prod/state.tfvars
-# 	terraform apply -auto-approve -var-file=env-prod/main.tfvars -var vault_token=$(vault_token)
-
-# prod-destroy:
-# 	git pull
-# 	rm -f .terraform/terraform.tfstate
-# 	terraform init -backend-config=env-prod/state.tfvars
-# 	terraform destroy -auto-approve -var-file=env-prod/main.tfvars -var vault_token=$(vault_token)
+secrets:
+	git pull
+	cd misc/vault-secrets ; make vault_token=$(vault_token)
--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -22,10 +22,94 @@ resource "vault_mount" "ssh" {
 
 resource "vault_generic_secret" "ssh" {
   path = "${vault_mount.ssh.path}/ssh"
+
   data_json = <<EOT
 {
   "username": "ec2-user",
   "password": "DevOps321"
+}
+EOT
+}
+
+resource "vault_mount" "roboshop-dev" {
+  path        = "roboshop-dev"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "Roboshop Dev secrets"
+}
+
+resource "vault_generic_secret" "roboshop-dev-cart" {
+  path = "${vault_mount.roboshop-dev.path}/cart"
+
+  data_json = <<EOT
+{
+"REDIS_HOST":   "redis-dev.rdevopsb83.online",
+"CATALOGUE_HOST": "catalogue-dev.rdevopsb83.online",
+"CATALOGUE_PORT": "8080"
+}
+EOT
+}
+
+resource "vault_generic_secret" "roboshop-dev-catalogue" {
+  path = "${vault_mount.roboshop-dev.path}/catalogue"
+
+  data_json = <<EOT
+{
+"MONGO":   "true",
+"MONGO_URL": "mongodb://mongodb-dev.rdevopsb83.online:27017/catalogue"
+}
+EOT
+}
+
+resource "vault_generic_secret" "roboshop-dev-frontend" {
+  path = "${vault_mount.roboshop-dev.path}/frontend"
+
+  data_json = <<EOT
+{
+"catalogue":   "http://catalogue-dev.rdevopsb83.online:8080/",
+"user":   "http://user-dev.rdevopsb83.online:8080/",
+"cart":   "http://cart-dev.rdevopsb83.online:8080/",
+"shipping":   "http://shipping-dev.rdevopsb83.online:8080/",
+"payment":   "http://payment-dev.rdevopsb83.online:8080/"
+}
+EOT
+}
+
+resource "vault_generic_secret" "roboshop-dev-payment" {
+  path = "${vault_mount.roboshop-dev.path}/payment"
+
+  data_json = <<EOT
+{
+"CART_HOST" : "cart-dev.rdevopsb83.online",
+"CART_PORT" : 8080,
+"USER_HOST" : "user-dev.rdevopsb83.online",
+"USER_PORT" : 8080,
+"AMQP_HOST" : "rabbitmq-dev.rdevopsb83.online",
+"AMQP_USER" : "roboshop",
+"AMQP_PASS" : "roboshop123"
+}
+EOT
+}
+
+resource "vault_generic_secret" "roboshop-dev-shipping" {
+  path = "${vault_mount.roboshop-dev.path}/shipping"
+
+  data_json = <<EOT
+{
+"CART_ENDPOINT" : "cart-dev.rdevopsb83.online:8080",
+"DB_HOST" : "mysql-dev.rdevopsb83.online"
+}
+EOT
+}
+
+resource "vault_generic_secret" "roboshop-dev-user" {
+  path = "${vault_mount.roboshop-dev.path}/user"
+
+  data_json = <<EOT
+{
+"MONGO" : "true",
+"REDIS_URL" : "redis://redis-dev.rdevopsb83.online:6379",
+"MONGO_URL" : "mongodb://mongodb-dev.rdevopsb83.online:27017/users"
 }
 EOT
 }
