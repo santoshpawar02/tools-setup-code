b8daee826b8e2982174bd639fd8b5495695a3e35
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-03-10 23:09:55 +0530

    shipping changes

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -73,7 +73,7 @@ resource "vault_generic_secret" "roboshop-dev-frontend" {
 "catalogue":   "http://catalogue-dev.santoshpawar.site:8080/",
 "user":   "http://user-dev.santoshpawar.site:8080/",
 "cart":   "http://cart-dev.santoshpawar.site:8080/",
-"shipping":   "http://shipping-dev.santoshpawar.site:8080/"
+"shipping":   "http://shipping-dev.santoshpawar.site:8080/",
 "CATALOGUE_HOST" : "catalogue-dev.santoshpawar.site",
 "CATALOGUE_PORT" : "8080",
 "USER_HOST" : "user-dev.santoshpawar.site",
